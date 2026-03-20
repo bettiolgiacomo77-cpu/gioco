745f131b49d87dbb8af15674831675f695cb075b
Giacomo Bettiol <bettiolgiacomo77@gmail.com>
2026-03-20 17:05:55 +0100

    percorso infinito

--- gioco_platformer.py
+++ gioco_platformer.py
@@ -1,30 +1,26 @@
 import arcade
-
+import random
 
 """  gioco platformer per scuola"""
-
-
-
-
-
-
-
 
 class giocoplatformer(arcade.Window):
 
     def __init__(self, larghezza, altezza, titolo):
         super().__init__(larghezza, altezza, titolo)
         
+        # oggetti
         self.babbo = None
         self.moneta = None
         self.lista_babbo = arcade.SpriteList()
         self.lista_moneta = arcade.SpriteList()
         
-        # muri
-        self.wall_list = None
         self.background = None
         self.suono_munch = arcade.load_sound("./assets/mangiare.mp3")
 
+        # muri
+        self.wall_list = None
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        
         # Movimento
         self.up_pressed = False
         self.down_pressed = False
@@ -38,6 +34,10 @@ class giocoplatformer(arcade.Window):
         
         #camera
         self.camera = arcade.camera.Camera2D()
+        
+        """..."""
+        # Variabile per tracciare fin dove abbiamo generato il terreno
+        self.ultimo_x_generato = 0
 
         self.setup()
 
@@ -48,70 +48,75 @@ class giocoplatformer(arcade.Window):
         self.babbo.center_y = 115
         self.lista_babbo.append(self.babbo)
         
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
-        
         self.background = arcade.load_texture("./assets/foresta.jpg")
-                
-        self.lista_moneta.clear()
         
-        # creazione pavimento
-        for x in range(0, 1250, 64):
+        """..."""
+        # Generazione iniziale del terreno (primi 2000 pixel)
+        self.genera_segmento_livello(0, 2000)
+        
+    """..."""    
+    def genera_segmento_livello(self, start_x, end_x):
+        """Genera terreno e oggetti in un intervallo X specifico"""
+        # Genera Pavimento
+        for x in range(start_x, end_x, 64):
             wall = arcade.Sprite("./assets/terra.png", 0.17)
             wall.center_x = x
             wall.center_y = 32
-            self.wall_list.append(wall)
+            self.wall_list.append(wall)    
             
-        # crea gli ostacoli
-        coordinate_list = [[250, 105], [500, 105], [750, 105]]
-        
-        for coordinate in coordinate_list:
-            wall = arcade.Sprite("./assets/tubo.png", 0.17)
-            wall.position = coordinate
-            self.wall_list.append(wall)
+        # generazione ostacoli casuali (tubi)    
+        for x in range(start_x + 300, end_x, 400):
+            if random.random() > 0.02: # 200% di probabilità di avere un tubo
+                wall = arcade.Sprite("./assets/tubo.png", 0.17)
+                wall.position = [x, 105]
+                self.wall_list.append(wall)
 
-
-        self.crea_moneta()
-
-        
-    def crea_moneta(self):
-        
-        coordinate_ = [[150, 105], [180, 105], [810, 105], [840,105], [870,105], [250,150]]
-        
-        for coordinate in coordinate_:
-            self.moneta = arcade.Sprite("./assets/grano.webp",0.2)
-            self.moneta.position = coordinate
-            self.lista_moneta.append(self.moneta)
+        # Genera grano in modo casuale
+        for x in range(start_x + 100, end_x, 150):
+            moneta = arcade.Sprite("./assets/grano.webp", 0.2)
+            moneta.position = [x, random.randint(100, 200)]
+            self.lista_moneta.append(moneta)
+            
+        self.ultimo_x_generato = end_x
 
     def on_draw(self):
                 
         self.clear()
 
+        # disegna lo sfondo
         arcade.draw_texture_rect(
             self.background,
-            arcade.LBWH(0, 0, 1060, 720)
+            arcade.LBWH(self.camera.position[0] - self.width/2, 0, 1060, 720)
         )
-        
-        self.camera
         
         self.lista_moneta.draw()
         
         self.lista_babbo.draw()
         
         self.wall_list.draw()
-
+        
+        # fa in modo che le scritte si muovano con la camera
+        coordinate_testo = self.camera.position[0] - self.width/2
         arcade.draw_text(
             f"Punteggio: {self.punteggio}",
-            20, 
+            coordinate_testo + 20,
             self.height - 30,
             arcade.color.WHITE, 15
-        )
+            )
         
         arcade.draw_text(
-            f"gioco platformer",
-            180,
+            f"Endless Runner",
+            coordinate_testo + 180,
             self.height - 30,
             arcade.color.BLACK, 15
-        )
+            )
+        
+        arcade.draw_text(
+            f"Distanza: {int(self.babbo.center_x)}m",
+            coordinate_testo + 450,
+            self.height - 30,
+            arcade.color.BLACK, 15
+            )
         
         self.camera.use()
 
@@ -124,7 +129,7 @@ class giocoplatformer(arcade.Window):
         
         self.camera.position = self.babbo.position
         
-        """gemini"""
+        """..."""
         # per non far vedere il vuoto 
         camera_x = max(self.camera.position[0], self.width / 2) 
         self.camera.position = (camera_x, self.height / 2)
@@ -138,6 +143,15 @@ class giocoplatformer(arcade.Window):
         if self.right_pressed:
             change_x += self.velocita
             
+        #genera nuovi pezzi di mappa    
+        if self.babbo.center_x > self.ultimo_x_generato - 1000:
+            self.genera_segmento_livello(self.ultimo_x_generato, self.ultimo_x_generato + 1000)
+            
+        # rimuove gli sprite gia passati
+        for sprite in self.wall_list:
+            if sprite.right < self.babbo.center_x - 1000:
+                sprite.remove_from_sprite_lists()
+            
         # Applica movimento
         
         self.babbo.center_x += change_x
@@ -148,18 +162,6 @@ class giocoplatformer(arcade.Window):
             self.babbo.scale = (-0.1, 0.1)
         elif change_x > 0:
             self.babbo.scale = (0.1, 0.1)
-        
-            
-        # Limita movimento dentro lo schermo
-        if self.babbo.center_x < 0:
-            self.babbo.center_x = 0
-        elif self.babbo.center_x > self.width:
-            self.babbo.center_x = self.width
-        
-        if self.babbo.center_y < 0:
-            self.babbo.center_y = 0
-        elif self.babbo.center_y > self.height:
-            self.babbo.center_y = self.height
 
         # Collisioni
         collisioni = arcade.check_for_collision_with_list(self.babbo, self.lista_moneta)
@@ -169,8 +171,11 @@ class giocoplatformer(arcade.Window):
             for moneta in collisioni:
                 moneta.remove_from_sprite_lists()
                 self.punteggio += 1
-
+                
+        
     def on_key_press(self, tasto, modificatori):
+    
+            
         if tasto in (arcade.key.UP, arcade.key.W):
             self.up_pressed = True
         elif tasto in (arcade.key.DOWN, arcade.key.S):
@@ -194,7 +199,7 @@ class giocoplatformer(arcade.Window):
 
 
 def main():
-    gioco = giocoplatformer(1060,720, "Babbo Natale")
+    gioco = giocoplatformer(920,620, "Babbo Natale")
     arcade.run()
 
 
